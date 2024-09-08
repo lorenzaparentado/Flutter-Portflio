7ab66dd584e1f7f411b0560fb34c27b407ecf9f1
Lorenz Aparentado <lorenzaparentado@gmail.com>
2024-09-07 23:51:51 -0400

    made landing responsive

--- flutter_portfolio/lib/home_page/landing.dart
+++ flutter_portfolio/lib/home_page/landing.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import '../styles/app_colors.dart';
 import '../styles/text_styles.dart';
+import '../tools.dart';
 
 class Landing extends StatefulWidget {
   final double screenWidth;
@@ -13,39 +14,38 @@ class Landing extends StatefulWidget {
 }
 
 class _Landing extends State<Landing> {
-  var idealHeight = 1024;
-  var idealWidth = 1440;
-  
   @override
   Widget build(BuildContext context) {
+    double imageWidth = widget.screenWidth >= 1440 ? 434 : widget.screenWidth * 0.3;
     return Container(
-      height: widget.screenHeight * (729 / idealHeight),
+      height: responsiveHeight(widget.screenHeight, 729),
       child: Stack(
         children: [
           Positioned(
-            top: widget.screenHeight * (216 / idealHeight),
-            left: widget.screenWidth * (100 / idealWidth),
+            top: responsiveHeight(widget.screenHeight, 216),
+            left: responsiveWidth(widget.screenWidth, 100),
             child: Text(
               "Lorenz\nAparentado",
               style: headerBiggest(AppColors.darkestBrown),
             ),
           ),
           Positioned(
-            top: widget.screenHeight * (182 / idealHeight),
-            left: (widget.screenWidth / 2) - 207 - 20,
-            child: Image.asset('assets/images/lorenzOutline.png'),
-          ),
+                bottom: 0,
+                left: (widget.screenWidth / 2) - (imageWidth / 2),
+                child: Image.asset(
+                  'assets/images/lorenz.png',
+                  width: imageWidth,
+                ),
+              ),
+
           Positioned(
-            top: widget.screenHeight * (162 / idealHeight),
-            left: (widget.screenWidth / 2) - 207,
-            child: Image.asset('assets/images/lorenz.png'),
-          ),
-          Positioned(
-            top: widget.screenHeight * (216 / idealHeight),
-            right: widget.screenWidth * (99 / idealWidth),
-            child: Text(
-              "Third Year Computer Science Student\nAspiring Software Engineer\nMobile / Fullstack Developer",
-              style: bodyBig(AppColors.darkestBrown),
+            top: responsiveHeight(widget.screenHeight, 216),
+            right: responsiveWidth(widget.screenWidth, 99),
+            child: Container(
+              child: Text(
+                "Third Year Computer Science Student\nAspiring Software Engineer\nMobile / Fullstack Developer",
+                style: bodyBig(AppColors.darkestBrown),
+              ),
             ),
           ),
         ],
