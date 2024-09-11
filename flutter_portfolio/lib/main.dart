f2842c3f0fb175b52d3e0159b5da37799155e3a7
Lorenz Aparentado <lorenzaparentado@gmail.com>
2024-09-10 21:16:39 -0400

    changed website title in tab

--- flutter_portfolio/lib/main.dart
+++ flutter_portfolio/lib/main.dart
@@ -1,6 +1,7 @@
 // main.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_portfolio/responsive.dart';
+import 'package:flutter_portfolio/styles/app_colors.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,9 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Lorenz Aparentado Portfolio',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: AppColors.lightTan),
         useMaterial3: true,
       ),
       home: const Responsive(),
